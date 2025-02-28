9f2f6e0c5fa4985ef40d67332a6273aa3693ca0e
timonso <tooloris@gmail.com>
2025-02-28 14:22:03 +0000

    before rename

--- demo_ik/Source/demo_ik/IK_Fabrik.cpp
+++ demo_ik/Source/demo_ik/IK_Fabrik.cpp
@@ -1,14 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "IK_Fabrik.h"
-#include "Math/UnrealMathVectorCommon.h"
 
 
 // Sets default values for this component's properties
 UIK_Fabrik::UIK_Fabrik()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
 	targetActor_reference = CreateDefaultSubobject<AActor>(TEXT("Actor"));
@@ -21,24 +18,29 @@ void UIK_Fabrik::Solve(UPoseableMeshComponent* skeleton, const FVector& targetPo
 	// check if the bone vectors are empty
 	if (boneNames.Num() == 0)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("no bone vectors found for FABRIK"));
+		UE_LOG(LogTemp, Warning, TEXT("no bone vectors found for CCD"));
 		return;
 	}
-
 
 	// iteratively approximate a solution
 	for (int i = 0; i < iterationCount; i++)
 	{
 		for (int b = 0; b < boneNames.Num(); b++) {
+			// end effector
 			FVector endBonePos = skeleton->GetBoneLocation(FName(boneNames[0],  EBoneSpaces::WorldSpace));
+
+			// check if the end effector is close enough to the target
 			if (FVector::Dist(endBonePos, targetPosition) < threshold) {
 				return;
 			}
+
 			FName currentBoneName = FName(boneNames[b]);
 			FVector currentBonePos = skeleton->GetBoneLocation(currentBoneName, EBoneSpaces::WorldSpace);
 			FQuat currentBoneRot = skeleton->GetBoneQuaternion(currentBoneName, EBoneSpaces::WorldSpace);
 			FVector targetDirection = (targetPosition - currentBonePos);
 			FVector endBoneDirection = (endBonePos - currentBonePos);
+
+			// determine and apply the appropriate rotation towards the target
 			FQuat newBoneRot = FQuat::FindBetweenVectors(endBoneDirection, targetDirection) * currentBoneRot;
 			skeleton->SetBoneRotationByName(currentBoneName, newBoneRot.Rotator(), EBoneSpaces::WorldSpace);
 		}
@@ -55,7 +57,7 @@ void UIK_Fabrik::BeginPlay()
 		PosableMesh = PosableCharacter->posableMeshComponent_reference;
 	}
 	else {
-		UE_LOG(LogTemp, Warning, TEXT("Fabrik: Poseable character not found"));
+		UE_LOG(LogTemp, Warning, TEXT("CCD: Poseable character not found"));
 	}
 }
 
